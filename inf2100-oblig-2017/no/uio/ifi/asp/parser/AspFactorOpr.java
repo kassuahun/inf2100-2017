e390e1f8dad29472b437905987d4012c23fe522b
Kassahun Gedlu Degena <kassuahun@gmail.com>
2017-10-06 00:53:59 +0200

    friday 00:53

--- inf2100-oblig-2017/no/uio/ifi/asp/parser/AspFactorOpr.java
+++ inf2100-oblig-2017/no/uio/ifi/asp/parser/AspFactorOpr.java
@@ -1,5 +1,10 @@
+package no.uio.ifi.asp.parser;
+
 import no.uio.ifi.asp.main.Main;
 import no.uio.ifi.asp.parser.AspSyntax;
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
 import no.uio.ifi.asp.scanner.Scanner;
 import no.uio.ifi.asp.scanner.TokenKind;
 
@@ -13,10 +18,7 @@ public class AspFactorOpr extends AspSyntax{
         super(lNum);
     }
 
-    @Override
-    protected void prettyPrint() {
 
-    }
 
     public static AspFactorOpr parse(Scanner s) {
         Main.log.enterParser("factor opr");
@@ -28,5 +30,13 @@ public class AspFactorOpr extends AspSyntax{
         Main.log.leaveParser("factor opr");
         return fOpr;
     }
+    @Override
+    protected void prettyPrint() {
 
+    }
+
+    @Override
+    RuntimeValue eval(RuntimeScope curScope) throws RuntimeReturnValue {
+        return null;
+    }
 }
