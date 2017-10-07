7bad73e4a0713f448b764b5c1218fd10480c1069
Kassahun Gedlu Degena <kassuahun@gmail.com>
2017-10-07 19:54:31 +0200

    1955

--- inf2100-oblig-2017/no/uio/ifi/asp/parser/AspSuite.java
+++ inf2100-oblig-2017/no/uio/ifi/asp/parser/AspSuite.java
@@ -1,4 +1,30 @@
 package no.uio.ifi.asp.parser;
 
-public class AspSuite {
+import no.uio.ifi.asp.main.Main;
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.scanner.Scanner;
+
+public class AspSuite extends AspSyntax{
+    AspSuite(int n) {
+        super(n);
+    }
+
+    public static AspSuite parse(Scanner s) {
+        Main.log.enterParser("AspSuite");
+
+        Main.log.leaveParser("AspSuite");
+        return null;
+    }
+
+    @Override
+    protected void prettyPrint() {
+
+    }
+
+    @Override
+    RuntimeValue eval(RuntimeScope curScope) throws RuntimeReturnValue {
+        return null;
+    }
 }
