9e49e5e84f2325daa2b2db1ada5c16bd36a86c4c
Kassahun Gedlu Degena <kassuahun@gmail.com>
2017-10-07 21:03:12 +0200

    2100

--- inf2100-oblig-2017/no/uio/ifi/asp/parser/AspStringLiteral.java
+++ inf2100-oblig-2017/no/uio/ifi/asp/parser/AspStringLiteral.java
@@ -1,4 +1,32 @@
 package no.uio.ifi.asp.parser;
 
-public class AspStringLiteral {
+import no.uio.ifi.asp.main.Main;
+import no.uio.ifi.asp.runtime.RuntimeReturnValue;
+import no.uio.ifi.asp.runtime.RuntimeScope;
+import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.scanner.Scanner;
+
+public class AspStringLiteral extends AspAtom{
+    AspStringLiteral(int n) {
+        super(n);
+    }
+
+    public static AspStringLiteral parse(Scanner s) {
+        Main.log.enterParser("AspStringLiteral");
+        AspStringLiteral strLit = new AspStringLiteral(s.curLineNum());
+
+
+        Main.log.leaveParser("AspStringLiteral");
+        return strLit;
+    }
+
+    @Override
+    protected void prettyPrint() {
+
+    }
+
+    @Override
+    RuntimeValue eval(RuntimeScope curScope) throws RuntimeReturnValue {
+        return null;
+    }
 }
