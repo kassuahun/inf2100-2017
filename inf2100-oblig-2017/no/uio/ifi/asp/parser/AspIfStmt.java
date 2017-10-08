e4dbd9de741834d8a8e6193dfc691b4b9d6af38d
Kassahun Gedlu Degena <kassuahun@gmail.com>
2017-10-08 17:49:18 +0200

    sunday 18

--- inf2100-oblig-2017/no/uio/ifi/asp/parser/AspIfStmt.java
+++ inf2100-oblig-2017/no/uio/ifi/asp/parser/AspIfStmt.java
@@ -2,21 +2,49 @@ package no.uio.ifi.asp.parser;
 
 import no.uio.ifi.asp.main.Main;
 import no.uio.ifi.asp.scanner.Scanner;
+import no.uio.ifi.asp.scanner.TokenKind;
+
+import java.util.ArrayList;
+
+import static no.uio.ifi.asp.scanner.TokenKind.*;
+import static no.uio.ifi.asp.scanner.TokenKind.ifToken;
 
 public class AspIfStmt extends AspStmt {
 
+    ArrayList<AspExpr> condList = new ArrayList<AspExpr>();
+    ArrayList<AspSuite> suteList = new ArrayList<AspSuite>();
+    AspSuite elsesute;
+
+    AspExpr condition;
+    // Statement thenStmt;
+    // Statement elseStmt; // optional
 
     public AspIfStmt(int i) {
         super(i);
     }
 
-    public static AspFuncDef parse(Scanner s) {
-        Main.log.enterParser("AspFuncDef");
-        AspFuncDef fOpr = new AspFuncDef(s.curLineNum());
+    public static AspIfStmt parse(Scanner s) {
+        Main.log.enterParser("AspIfStmt");
+        AspIfStmt ifStmt = new AspIfStmt(s.curLineNum());
 
+        skip(s, ifToken);
 
+        while (true){
+            ifStmt.condList.add(AspExpr.parse(s));
+            skip(s,colonToken);
+            ifStmt.suteList.add(AspSuite.parse(s));
+            if(s.curToken().kind != elifToken) break;
+            skip(s, elifToken);
+        }
+        if (s.curToken().kind == elifToken)
+        {
+            skip(s, elseToken);
+            skip(s,colonToken);
+            //ifStmt.suteList.add(AspSuite.parse(s));
+            ifStmt.elsesute = AspSuite.parse(s);
+        }
 
-        Main.log.leaveParser("AspFuncDef");
+        Main.log.leaveParser("AspIfStmt");
         return null;
     }
 }
